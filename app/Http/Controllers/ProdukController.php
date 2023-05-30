2c58f241fcdd031ab35e02252f7d2d1e7a3aa553
Raihan <muhraihan185@gmail.com>
2023-05-30 12:04:31 +0700

    add dashboard and produk controllers

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class DashboardController extends Controller
+class ProdukController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -12,7 +12,7 @@ class DashboardController extends Controller
     public function index()
     {
         //
-        return view('admin.index');
+        return view('admin.produk.index');
     }
 
     /**
